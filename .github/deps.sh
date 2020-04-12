150ac3954ca653fd0380fad8aa67da176e91d2b7
chyroc <chyroc@qq.com>
2020-04-12 23:39:27 +0800

    fix

--- .github/deps.sh
+++ .github/deps.sh
@@ -6,13 +6,15 @@ set -o pipefail
 # https://pycryptodome.readthedocs.io/en/latest/src/installation.html
 if [  -n "$(uname -a | grep Ubuntu)" ]; then
     if [[ `python -c "import sys; print(sys.version)"` == *"PyPy"* ]]; then
-      sudo apt-get install build-essential pypy-dev
+      sudo apt-get install libssl-dev build-essential pypy-dev
     else
       sudo apt-get install build-essential python-dev
     fi
 
     pip install pycryptodomex
     python -m Cryptodome.SelfTest
-else
-    echo "not ubuntu"
+fi
+
+if [  -n "$(uname -a | grep Darwin)" ]; then
+    brew install openssl
 fi
